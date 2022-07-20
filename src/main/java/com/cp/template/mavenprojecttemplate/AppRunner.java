a0320048055589cb1c381b74412bdf4d342a8e63
claypigeon123 <n.zoltan18@gmail.com>
2022-07-20 23:10:48 +0200

    logback template adjustment

--- src/main/java/com/cp/template/mavenprojecttemplate/AppRunner.java
+++ src/main/java/com/cp/template/mavenprojecttemplate/AppRunner.java
@@ -1,7 +1,12 @@
 package com.cp.template.mavenprojecttemplate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppRunner {
+    private static final Logger log = LoggerFactory.getLogger(AppRunner.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        log.info("{}", "Hello!");
     }
 }

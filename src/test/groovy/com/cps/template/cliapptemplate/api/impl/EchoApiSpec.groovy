f49aea6adff968f39818d1fbf3cb5bad4816a41d
claypigeon123 <n.zoltan18@gmail.com>
2023-05-27 14:28:00 +0200

    better test

--- src/test/groovy/com/cps/template/cliapptemplate/api/impl/EchoApiSpec.groovy
+++ src/test/groovy/com/cps/template/cliapptemplate/api/impl/EchoApiSpec.groovy
@@ -38,11 +38,21 @@ class EchoApiSpec extends Specification {
     }
 
     def "verify fails if no message provided"() {
+        given:
+        def expected = """\
+            Missing required option: '--message=<message>'
+            Usage: echo [-hV] -m=<message>
+            -> displays a provided message in standard output
+              -h, --help                Show this help message and exit.
+              -m, --message=<message>   Message to print
+              -V, --version             Print version information and exit.\
+        """.stripIndent().replaceAll("\n", System.lineSeparator())
+
         when:
         int exitCode = cmd.execute()
 
         then:
         exitCode == ExitCode.USAGE
-        output.err.trim().contains("Missing required option: '--message=<message>'")
+        output.err.trim() == expected
     }
 }
